fbdc605c9f4e9a5b87c742b20483fa1405f4ffcb
Yunus Celik <21yunuscelik21@gmail.com>
2024-04-04 18:14:51 +0300

    Simulation and truck class added.

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -2,29 +2,28 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <cmath>
 #define print(x) std::cout << x << std::endl
 
-// returns an int array of given size with random values between 10 (inclusive) and 100 (exclusive)
 int* generate_random_array(int size) {
 	if (size <= 0)
 		return nullptr;
 
-	srand((unsigned int)time(NULL));
 	int* arr = new int[size];
 
 	for (int i = 0; i < size; ++i) 
-		arr[i] = std::rand() % 90 + 10;
+		arr[i] = std::rand();
 	
 	return arr;
 }
 
-void quick_sort(int*& arr, const int begin, const int end, int& time);
+void quick_sort(int*& arr, const int begin, const int end, double& time);
 
 int* generate_almost_sorted_array(int size) {
 	srand((unsigned int)time(NULL));
 
 	int* random_array = generate_random_array(size);
-	int time;
+	double time = 0;
 	quick_sort(random_array, 0, size - 1, time);
 
 		int swap_count = size * 0.03;
@@ -44,29 +43,25 @@ void swap(int* arr, const int& x, const int& y) {
 	arr[y] = temp;
 }
 
-void swap(std::vector<int>& arr, const int& x, const int& y) {
-	int temp = arr[x];
-	arr[x] = arr[y];
-	arr[y] = temp;
-}
-
-void insertion_sort(int*& arr, const int& size) {
+double insertion_sort(int*& arr, const int size) {
 	int time = 0;
 	for (int i = 1; i < size; ++i) {
 
 		int temp = arr[i];
-		for (int j = i - 1; j >= 0 && temp < arr[j]; --j) {
+		int j = i - 1;
+		for (; j >= 0 && temp < arr[j]; --j) {
 			arr[j + 1] = arr[j];
 			arr[j] = temp;
-			time += 11;
+			time += 10 + 1;
 		}
-		++time;
+		if (j >= 0)
+			++time;
 	}
 
-	print("Time: " << time);
+	return time;
 }
 
-void selection_sort(int*& arr, const int& size) {
+double selection_sort(int*& arr, const int size) {
 	int time = 0;
 	for (int i = 0; i < size - 1; ++i) {
 		int min = INT_MAX;
@@ -82,10 +77,10 @@ void selection_sort(int*& arr, const int& size) {
 		time += 15;
 	}
 
-	print("Time: " << time);
+	return time;
 }
 
-void bubble_sort(int*& arr, const int& size) {
+double bubble_sort(int*& arr, const int size) {
 	int time = 0;
 	for (int i = size - 1; i > 0; --i) {
 		for (int j = 0; j < i; ++j) {
@@ -97,10 +92,10 @@ void bubble_sort(int*& arr, const int& size) {
 		}
 	}
 
-	print("Time: " << time);
+	return time;
 }
 
-int partition(int*& arr, int begin, int end, int& time) {
+int partition(int*& arr, int begin, int end, double& time) {
 	int piv = begin;
 	int pivot = arr[piv];
 	++begin;
@@ -114,46 +109,93 @@ int partition(int*& arr, int begin, int end, int& time) {
 			++begin;
 			++time;
 		}
-		time += 2;
+
 		if (begin < end) {
 			swap(arr, begin, end);
-			time += 15;
+			time += 15 + 1 + 1;
 		}
 		else {
 			swap(arr, piv, end);
-			time += 15;
+			time += 15 + 1;
 		}
 	}
 
 	return end;
 }
 
-void quick_sort(int*& arr, const int begin, const int end, int& time) {
+void quick_sort(int*& arr, const int begin, const int end, double& time) {
 	if (begin < end) {
 		int pos = partition(arr, begin, end, time);
 
 		quick_sort(arr, begin, pos - 1, time);
 		quick_sort(arr, pos + 1, end, time);
 	}
-
 }
 
+class Truck {
+public:
+    int* trees;
+	int size;
+
+	Truck() : trees(nullptr), size(0) {}
+	Truck(int size) {
+		trees = generate_random_array(size);
+		this->size = size;
+	}
+	~Truck() {
+		delete[] trees;
+	}
+
+	void setTrees(int size) {
+		if (trees)
+			delete[] trees;
+
+		if (size > 0) {
+			trees = generate_random_array(size);
+			this->size = size;
+		}
+		else {
+			trees = nullptr;
+			this->size = 0;
+		}
+	}
+
+	void shuffle() {
+		if (trees) {
+			delete[] trees;
+			trees = generate_random_array(size);
+		}
+	}
+}; 
+
+
 int main() {
-	int* arr1 = generate_almost_sorted_array(1500);
-	int time = 0;
-	quick_sort(arr1, 0, 1499, time);
-	
-	print("Time: " << time);
+	srand((unsigned int)time(NULL));
+	Truck* trucks = new Truck[10];
 
-	int* arr2 = generate_almost_sorted_array(1500);
-	selection_sort(arr2, 1500);
-	
-	int* arr3 = generate_almost_sorted_array(1500);
-	insertion_sort(arr3, 1500);
+	for (int i = 0; i < 10; ++i) {
+		trucks[i].setTrees(pow(2, i + 3));
+	}
 
-	int* arr4 = generate_almost_sorted_array(1500);
-	bubble_sort(arr4, 1500);
+	for (int i = 0; i < 10; ++i) {
+		print("Truck " << i + 1 << " with size " << trucks[i].size);
+		double inser = 0, selec = 0, bubb = 0, quick = 0;
+		for (int j = 0; j < 5; ++j) {
+			inser += insertion_sort(trucks[i].trees, trucks[i].size);
+			trucks[i].shuffle();
 
-	
-	return 0;
+			selec += selection_sort(trucks[i].trees, trucks[i].size);
+			trucks[i].shuffle();
+
+			bubb += bubble_sort(trucks[i].trees, trucks[i].size);
+			trucks[i].shuffle();
+
+			double time = 0;
+			quick_sort(trucks[i].trees, 0, trucks[i].size - 1, time);
+			trucks[i].shuffle();
+			quick += time;
+		}
+		print(inser / 5 << " " << selec / 5 << " " << bubb / 5 << " " << quick / 5 << "\n");
+	}
+
 }
